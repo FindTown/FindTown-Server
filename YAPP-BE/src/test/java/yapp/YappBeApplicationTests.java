eae9846b13cb5da48fee5c9ae913f1f7e4c16c01
유지훈 <dbwlgna98@naver.com>
2022-12-04 12:05:12 +0900

    feat : project 초기설정

---
+++ YAPP-BE/src/test/java/yapp/YappBeApplicationTests.java
@@ -0,0 +1,11 @@
+package yapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YappBeApplicationTests {
+//	@Test
+//	void contextLoads() {
+//	}
+}
