f8a15d0e431d785486fe9af1ec3b0a825b6e45e1
유지훈 <dbwlgna98@naver.com>
2022-12-04 11:57:51 +0900

    feat : XssConfig 설정

---
+++ YAPP-BE/src/main/java/yapp/common/config/XssConfig.java
@@ -0,0 +1,20 @@
+package yapp.common.config;
+
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class XssConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new XssEscapeServletFilter());
+        filterRegistration.setOrder(1);
+        filterRegistration.addUrlPatterns("/*");
+        return filterRegistration;
+    }
+}
