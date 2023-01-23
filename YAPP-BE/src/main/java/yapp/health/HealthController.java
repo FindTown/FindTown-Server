04dbc48f794e974b1208a7ed72a55d72f0d5d483
유지훈 <dbwlgna98@naver.com>
2023-01-23 19:09:40 +0900

    [#16]Fix: 헬스체크 Api 접근 권한 수정

--- YAPP-BE/src/main/java/yapp/health/HealthController.java
+++ YAPP-BE/src/main/java/yapp/health/HealthController.java
@@ -2,9 +2,10 @@ package yapp.health;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yapp.common.response.ApiResponse;
+import yapp.common.response.ApiResponseHeader;
 
 @Tag(name = "네트워크 연결 체크")
 @RestController
@@ -12,7 +13,7 @@ public class HealthController {
 
   @Operation(summary = "헬스 체크")
   @GetMapping("/health")
-  public HttpMethod getCheck() {
-    return HttpMethod.GET;
+  public ApiResponse getCheck() {
+    return new ApiResponse(new ApiResponseHeader(200, "네트워크 연결 성공!!!"), null);
   }
 }
