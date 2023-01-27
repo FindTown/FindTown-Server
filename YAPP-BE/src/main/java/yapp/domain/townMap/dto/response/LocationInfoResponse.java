e5f7ca407e65032329a93ae557b46ed0a0a34c03
gkim <kimkm0419@naver.com>
2023-01-28 00:22:46 +0900

    [#10]Feat: 동네 지도 좌표 조회 api Resp DTO 생성

---
+++ YAPP-BE/src/main/java/yapp/domain/townMap/dto/response/LocationInfoResponse.java
@@ -0,0 +1,14 @@
+package yapp.domain.townMap.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class LocationInfoResponse {
+  private Long object_id;
+  private String adm_nm;
+  private String[][] coordinates;
+}
