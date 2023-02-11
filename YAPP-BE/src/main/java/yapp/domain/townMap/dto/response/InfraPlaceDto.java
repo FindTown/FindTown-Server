feaa2f4076c6845151f6835b72c73583cd08b7fa
gkim <kimkm0419@naver.com>
2023-02-11 16:04:46 +0900

    [#20]Feat: Create InfraPlaceDto

---
+++ YAPP-BE/src/main/java/yapp/domain/townMap/dto/response/InfraPlaceDto.java
@@ -0,0 +1,31 @@
+package yapp.domain.townMap.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yapp.domain.townMap.entity.Place;
+
+@Schema(description = "인프라 장소 정보")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InfraPlaceDto {
+
+  @Schema(description = "장소 이름")
+  private String name;
+
+  @Schema(description = "장소 x 좌표")
+  private Long x;
+
+  @Schema(description = "장소 y 좌표")
+  private Long y;
+
+  @Builder
+  public InfraPlaceDto(Place place){
+    this.name = place.getName();
+    this.x = place.getX();
+    this.y = place.getY();
+  }
+  
+}
