ad4254f6df9946f3e3b24e62bf95cfc5c08d7884
gkim <kimkm0419@naver.com>
2023-02-11 16:13:05 +0900

    [#20]Feat: Create getInfraPlaceInfro in TownMapController

--- YAPP-BE/src/main/java/yapp/domain/townMap/controller/TownMapController.java
+++ YAPP-BE/src/main/java/yapp/domain/townMap/controller/TownMapController.java
@@ -4,9 +4,11 @@ import static yapp.common.config.Const.NON_USER;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yapp.common.response.ApiResponse;
@@ -16,17 +18,12 @@ import yapp.domain.townMap.service.TownMapService;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("app/townMap")
-@Tag(name = "[화면]-동네지도")
+@Tag(name = "[화면]-동네지도", description = "동네지도 API")
 public class TownMapController {
 
   private final TownMapService townMapService;
-
-  public TownMapController(
-    TownMapService townMapService
-  ){
-    this.townMapService = townMapService;
-  }
 
   @GetMapping("/location")
   @Operation(summary = "동네 지도 좌표 조회")
@@ -46,6 +43,16 @@ public class TownMapController {
       member_id);
 
     return ApiResponse.success("location-info", locationinfoResponse);
+  }
+
+  @GetMapping("/location/{object_id}/infra/{category}")
+  @Operation(summary = "동네 인프라 장소 조회")
+  public ApiResponse getInfraPlaceInfo(
+    @PathVariable Long object_id,
+    @PathVariable String category
+  ){
+
+    return ApiResponse.success(townMapService.getInfraPlaceInfo(object_id, category));
   }
 
 }
