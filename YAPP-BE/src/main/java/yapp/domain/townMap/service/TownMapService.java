d5b1f0bd857953272ec1c804341926ca5ddf748b
gkim <kimkm0419@naver.com>
2023-02-15 23:47:55 +0900

    [#38]Refactor: Change TownMapService for new infraPlace api response format

--- YAPP-BE/src/main/java/yapp/domain/townMap/service/TownMapService.java
+++ YAPP-BE/src/main/java/yapp/domain/townMap/service/TownMapService.java
@@ -5,6 +5,7 @@ import static yapp.common.config.Const.NON_USER;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,12 @@ import yapp.common.repository.LocationRepository;
 import yapp.domain.member.entity.MemberWishTown;
 import yapp.domain.member.repository.MemberWishTownRepository;
 import yapp.domain.townMap.converter.LocationConverter;
-import yapp.domain.townMap.dto.response.InfraPlaceDto;
+import yapp.domain.townMap.converter.PlaceConverter;
+import yapp.domain.townMap.dto.InfraPlaceDto;
 import yapp.domain.townMap.dto.response.LocationInfoResponse;
-import yapp.domain.townMap.dto.response.ThemePlaceDto;
+import yapp.domain.townMap.dto.ThemePlaceDto;
+import yapp.domain.townMap.dto.response.ThemePlaceResponse;
 import yapp.domain.townMap.entity.Infra;
-import yapp.domain.townMap.entity.Theme;
 import yapp.domain.townMap.repository.InfraRepository;
 import yapp.domain.townMap.repository.PlaceRepository;
 import yapp.domain.townMap.repository.ThemeRepository;
@@ -35,6 +37,7 @@ public class TownMapService {
   private final ThemeRepository themeRepository;
   private final PlaceRepository placeRepository;
   private final LocationConverter locationConverter;
+  private final PlaceConverter placeConverter;
 
   public LocationInfoResponse getLocationInfo(
     String memberId,
@@ -91,27 +94,21 @@ public class TownMapService {
     return infraPlaceListHashMap;
   }
 
-  public HashMap<String, Object> getThemePlaceInfo(
+  public HashMap<String, List<ThemePlaceResponse>> getThemePlaceInfo(
     Long object_id,
     String category
   ) {
 
-    HashMap<String, Object> listHashMap = new HashMap<>();
+    HashMap<String, List<ThemePlaceResponse>> themePlaceListHashMap = new HashMap<>();
 
-    List<Theme> themeList = themeRepository.findThemeByCategory(category);
+    List<ThemePlaceResponse> themePlaceList = placeRepository.findByTheme(object_id, category)
+      .stream()
+      .map(placeConverter::toThemePlace)
+      .collect(Collectors.toList());
 
-    for (int i = 0; i < themeList.size(); i++) {
-      List<ThemePlaceDto> themePlaceList = placeRepository.findByTheme(
-          object_id, category, themeList.get(i).getSubCategory())
-        .stream()
-        .map(ThemePlaceDto::new)
-        .collect(Collectors.toList());
-      ;
+    themePlaceListHashMap.put("placeList", themePlaceList);
 
-      listHashMap.put(themeList.get(i).getSubCategoryName(), themePlaceList);
-    }
-
-    return listHashMap;
+    return themePlaceListHashMap;
   }
 
 }
