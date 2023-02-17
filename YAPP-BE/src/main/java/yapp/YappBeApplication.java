24e09cbc4a05fac61d78e0d7750d69dff701e79b
유지훈 <dbwlgna98@naver.com>
2023-02-17 23:06:57 +0900

    [#52]Feat: Application 실행 시 거주자 통계 데이터 분석후 동네 데이터 설명란 추가

--- YAPP-BE/src/main/java/yapp/YappBeApplication.java
+++ YAPP-BE/src/main/java/yapp/YappBeApplication.java
@@ -3,11 +3,14 @@ package yapp;
 import java.util.Date;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import yapp.common.config.AppProperties;
 import yapp.common.config.CorsProperties;
+import yapp.common.service.ResidentStatisticsService;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ CorsProperties.class, AppProperties.class })
@@ -20,5 +23,13 @@ public class YappBeApplication {
   public void start() {
     TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     System.out.println("Now: " + new Date());
+  }
+
+  @Bean
+  public CommandLineRunner run(ResidentStatisticsService residentStatisticsService)
+    throws Exception {
+    return (String[] args) -> {
+      residentStatisticsService.setResidentStatisticsData();
+    };
   }
 }
