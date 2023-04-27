be50e7f9c7c58e4df030df5a975eb8eb9b2cd167
유지훈 <dbwlgna98@naver.com>
2023-04-27 15:39:39 +0900

    Refactor: Swagger config 운영 환경시 IP 주소 설정 변경

--- YAPP-BE/src/main/java/yapp/common/config/OpenAPIConfig.java
+++ YAPP-BE/src/main/java/yapp/common/config/OpenAPIConfig.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.models.servers.Server;
 import java.util.Arrays;
 import java.util.List;
 import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,10 +22,14 @@ public class OpenAPIConfig {
 
   private static final String PROD = "prod";
   private static final String LOCAL = "local";
+
+  @Value("${prod.host}")
+  private String PROD_HOST;
+
   private final Environment environment;
 
   public OpenAPIConfig(
-    Environment environment
+      Environment environment
   ) {
     this.environment = environment;
   }
@@ -35,60 +40,60 @@ public class OpenAPIConfig {
       if (Arrays.asList(environment.getActiveProfiles()).contains(PROD)) {
         Server prodServer = new Server();
         prodServer.setDescription(PROD);
-        prodServer.setUrl("https://townscoop.site/");
+        prodServer.setUrl(PROD_HOST);
 
         openAPI.info(
-            new Info()
-              .title("TownScoop API")
-              .version(buildProperties.getVersion())
-              .termsOfService("https://townscoop.site/")
-              .license(
-                new License().name("Apache 2.0").url("http://springdoc.org")
-              )
-          )
-          .servers(List.of(prodServer))
-          .components(
-            openAPI.getComponents().addSecuritySchemes(
-              "bearer",
-              new SecurityScheme()
-                .type(Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization")
+                new Info()
+                    .title("TownScoop API")
+                    .version(buildProperties.getVersion())
+                    .termsOfService(PROD_HOST)
+                    .license(
+                        new License().name("Apache 2.0").url("http://springdoc.org")
+                    )
             )
-          )
-          .addSecurityItem(
-            new SecurityRequirement().addList(
-              "bearer", Arrays.asList("read", "write")
+            .servers(List.of(prodServer))
+            .components(
+                openAPI.getComponents().addSecuritySchemes(
+                    "bearer",
+                    new SecurityScheme()
+                        .type(Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization")
+                )
             )
-          );
+            .addSecurityItem(
+                new SecurityRequirement().addList(
+                    "bearer", Arrays.asList("read", "write")
+                )
+            );
       } else if (Arrays.asList(environment.getActiveProfiles()).contains(LOCAL)) {
         openAPI.info(
-            new Info()
-              .title("TownScoop API")
-              .version(buildProperties.getVersion())
-              .termsOfService("http://localhost:8080/")
-              .license(
-                new License().name("Apache 2.0").url("http://springdoc.org")
-              )
-          )
-          .components(
-            openAPI.getComponents().addSecuritySchemes(
-              "bearer",
-              new SecurityScheme()
-                .type(Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization")
+                new Info()
+                    .title("TownScoop API")
+                    .version(buildProperties.getVersion())
+                    .termsOfService("http://localhost:8080/")
+                    .license(
+                        new License().name("Apache 2.0").url("http://springdoc.org")
+                    )
             )
-          )
-          .addSecurityItem(
-            new SecurityRequirement().addList(
-              "bearer", Arrays.asList("read", "write")
+            .components(
+                openAPI.getComponents().addSecuritySchemes(
+                    "bearer",
+                    new SecurityScheme()
+                        .type(Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization")
+                )
             )
-          );
+            .addSecurityItem(
+                new SecurityRequirement().addList(
+                    "bearer", Arrays.asList("read", "write")
+                )
+            );
       } else {
         openAPI.setComponents(new Components());
         openAPI.setPaths(new Paths());
